96970dc3397c3a8c0670be2b3f80e3717a3419df
ankur <argupy@gmail.com>
2019-06-06 17:54:45 +0530

    this commit by golu
    Signed-off-by: ankur <argupy@gmail.com>

---
+++ app/src/main/java/com/example/firstexample/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.firstexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button ex1, ex2, ex3, ex4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ex1 = findViewById(R.id.button1);
+        ex2 = findViewById(R.id.button2);
+        ex3 = findViewById(R.id.button3);
+        ex4 = findViewById(R.id.button4);
+
+
+        ex1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        Main2Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        ex2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        Main3Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        ex3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        Main4Activity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        ex4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        Main5Activity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+
+}
